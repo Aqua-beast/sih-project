223f4980207327e5a7468c65003a0d7b4e8ee02e
Aryan Sadh <sadharyan3@gmail.com>
2023-09-11 19:13:59 +0530

    Restored deleted files

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Sentiment from './components/Sentiment';
+
+function App() {
+  return (
+    <div className="App">
+      <Sentiment />
+    </div>
+  );
+}
+
+export default App;
